f2787b71b962b65565c9194616e4fe55c678089d
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-03 14:34:41 +0700

    feat: add slugs to posts page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,13 +2,15 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     @foreach ($posts as $post)
         <article class="max-w-screen-md py-8 border-b border-gray-300">
-            <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+            </a>
             <div class="text-base text-gray-500">
                 <a href="#">{{ $post['author'] }}</a> | {{$post['date']}}
             </div>
-            <p class="my-4 font-light">{{ $post['content'] }}
+            <p class="my-4 font-light">{{ Str::limit($post['content'], 150) }}
             </p>
-            <a href="" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
         </article>
     @endforeach
 </x-layout>
